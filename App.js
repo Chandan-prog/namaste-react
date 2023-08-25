45ee724bd64685b2aa01e3a6341bd56e717f403b
Chandan Kumar Mishra <chandankumarmishra@Chandans-MacBook-Air.local>
2023-08-25 21:13:55 +0530

    Removing commented code

--- App.js
+++ App.js
@@ -105,26 +105,3 @@ const App = () => {
 }
 root.render(<App/>);
 
-/* Lecture 1 and 2
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading", dataheading: "heading-name" }, //dataheading is a custom attribute that we can give
-  "Hello world from React!"
-); //creates a h1 tag with content
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am h1"),
-    React.createElement("h2", {}, "I am h2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 from child 2"),
-    React.createElement("h2", {}, "I am h2 from child 2"),
-  ])]
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root")); //creates a root element and remember you can have only one root element
-root.render(parent); //Renders/Injects the heading inside the root element */
