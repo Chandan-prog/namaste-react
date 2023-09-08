d48102a4c7d5f7d703f29823770d1d7827c221a8
Chandan Kumar Mishra <chandankumarmishra@Chandans-MacBook-Air.local>
2023-09-08 20:44:55 +0530

    Removing class based components

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,29 +1,10 @@
 import React from "react";
 import UserCard from "./UserCard";
-import UserClass from "./UserClass";
 
-// const About = () => {
-//     return(<div>
-//         <h1>This is about us page</h1>
-//         <UserCard/>
-//         <UserClass/>
-//     </div>)
-// }
-
-class About extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {
-    console.log("parent mounted");
-  }
-  render() {
-    return (
-      <div>
+const About = () => {
+    return(<div>
         <h1>This is about us page</h1>
-        <UserClass />
-      </div>
-    );
-  }
+        <UserCard/>
+    </div>)
 }
 export default About;
